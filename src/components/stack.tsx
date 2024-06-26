dc70edc5638ba8755df0e58bf5147df2a41b8d6a
saaxena <adarshsaxenaa@outlook.com>
2024-06-27 00:57:23 +0530

    stack

--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -62,6 +62,13 @@ const image = [
     designation: "Director",
     image: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
   },
+  {
+    id: 3,
+    name: 'Andre Gomez',
+    designation: 'Drumming Expert',
+    image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
+  },
+
 ];
 
 const CardStackDemo: React.FC = () => {
