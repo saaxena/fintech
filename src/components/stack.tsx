b6b4b59fe68da6d9b9f4c36f8200f6317ca3cc39
saaxena <adarshsaxenaa@outlook.com>
2024-06-27 00:46:10 +0530

    Fix type errors in Navbar and CardStackDemo components

--- src/components/stack.tsx
+++ src/components/stack.tsx
@@ -1,5 +1,3 @@
-// components/CardStackDemo.tsx
-
 import React from 'react';
 import { CardStack } from './ui/card-stack';
 import { AnimatedTooltip } from "./ui/animated-tooltip";
@@ -47,28 +45,22 @@ const CARDS = [
 
 const image = [
   {
-    id: 1,
+    id: 0,
     name: "Aditya Mohan",
     designation: "Operations Manager",
     image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
   },
   {
+    id: 1,
     name: "Rohit Kumar",
     designation: "CEO",
-    
     image: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80',
   },
   {
-    id: 3,
+    id: 2,
     name: "Abhishek Raza",
     designation: "Director",
     image: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
-  },
-  {
-    id: 4,
-    name: 'Andre Gomez',
-    designation: 'Drumming Expert',
-    image: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60',
   },
 ];
 
